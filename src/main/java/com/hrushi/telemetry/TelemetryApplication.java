16b8d83e3e0d5e1012c978e6968ec8b74d67dc7e
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2026-01-10 16:54:16 +0530

    init

---
+++ src/main/java/com/hrushi/telemetry/TelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.hrushi.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TelemetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrushi/telemetry/TelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrushi.telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
