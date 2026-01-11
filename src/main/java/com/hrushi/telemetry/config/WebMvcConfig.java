30f07533b96d8c5d5a8f526acf9d865c5cdaf67f
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2026-01-11 14:42:56 +0530

    add path prefix for REST controllers

--- src/main/java/com/hrushi/telemetry/config/WebMvcConfig.java
+++ src/main/java/com/hrushi/telemetry/config/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.hrushi.telemetry.config;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
@@ -9,5 +12,10 @@ class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configureApiVersioning(ApiVersionConfigurer configurer) {
         configurer.useRequestHeader("Api-Version");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
     }
 }
